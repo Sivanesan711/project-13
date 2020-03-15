cd391a8a8aadd801447c5165baed4b369fb00050
SIVANESAN-DHANIKACHALAM <61906309+Sivanesan711@users.noreply.github.com>
2020-03-15 08:49:05 +0530

    Add files via upload

---
+++ project-13-superwars-array-master/src/app.js
@@ -0,0 +1,63 @@
+  const PLAYERS = [
+      "Spiderman",
+      "Captain America",
+      "Wonderwoman",
+      "Popcorn",
+      "Gemwoman",
+      "Bolt",
+      "Antwoman",
+      "Mask",
+      "Tiger",
+      "Captain",
+      "Catwoman",
+      "Fish",
+      "Hulk",
+      "Ninja",
+      "Black Cat",
+      "Volverine",
+      "Thor",
+      "Slayer",
+      "Vader",
+      "Slingo"
+  ];
+  
+  // initialize players with image and strength
+  const initPlayers = (players) => {
+      return players.map((player, i) => {
+          return {
+              name: player,
+              image: "images/super  "    (i    1)    ".png",
+              strength: getRandomStrength(),
+              type: (Math.ceil(Math.random() * 109) % 2 == 0) ? "hero" : "villain"
+          }
+      });
+  }
+  
+  // getting random strength
+  const getRandomStrength = () => {
+      return Math.floor(Math.random() * 100);
+  }
+  
+  // Build player template
+  const buildPlayers = (players, type) => {
+      return players
+          .filter(player => player.type == type)
+          .map(player =>
+              `<div class="player">
+                  <img src="${player.image}" alt="">
+                  <div class="name">${player.name}</div>
+                  <div class="strength">${player.strength}</div>
+              </div>`)
+          .join();
+  }
+  
+  // Display players in HTML
+  const viewPlayers = (players) => {
+      document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+      document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+  }
+  
+  
+  window.onload = () => {
+      viewPlayers(initPlayers(PLAYERS));
+  }
